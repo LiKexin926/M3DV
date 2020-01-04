e3d0c4ab6448c5e3c356ed2a4c94ffecb9d69b11
LiKexin926 <57997566+LiKexin926@users.noreply.github.com>
2020-01-04 22:05:52 +0800

    Update test.py

--- DenseNet/test.py
+++ DenseNet/test.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 test_path = '../test'
 csv_filepath = 'submission.csv'
+res_path = 'submission.csv'
 new_test = read_d.new_test(csv_filepath,test_path)
 model = load_model('model.h5')
 y_pred = model.predict(new_test)[:,1]
@@ -20,4 +21,4 @@ for cont in csv_file:
     if cont[0]!='Id':
         name.append(cont[0])
 dataframe = pd.DataFrame({'Id':name,'Predicted':y_pred})
-dataframe.to_csv('submission.csv',index=False)
+dataframe.to_csv(res_path,index=False)
