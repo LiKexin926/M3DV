ec648b76852f53d8c8744882c56514a1f91abbfb
LiKexin926 <57997566+LiKexin926@users.noreply.github.com>
2020-01-04 22:11:20 +0800

    Update train.py

--- DenseNet/train.py
+++ DenseNet/train.py
@@ -7,13 +7,13 @@ import keras
 
 train_path = "../train_val" 
 label_path = "../train_val.csv"
+final_output_dir = "model.h5"
 X_train_new,x_val, train_label, y_val=read_d.new_data(train_path,label_path)
 model = get_model(weights=None)
 model.compile(loss='categorical_crossentropy', optimizer='adamax',metrics=["accuracy"])
 
-filepath="model.h5"
 reduce_lr = ReduceLROnPlateau(monitor='acc', factor=0.2,patience=3, min_lr=0.001)
-checkpoint = ModelCheckpoint(filepath,monitor='val_loss',verbose=0,save_best_only=True,mode='min',period=1)
+checkpoint = ModelCheckpoint(final_output_dir,monitor='val_loss',verbose=0,save_best_only=True,mode='min',period=1)
 callbacks_list=[reduce_lr,
                 checkpoint,
                ]
