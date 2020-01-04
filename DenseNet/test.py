5a3c322be4b2741917f6aaccf31b5c0582068280
LiKexin926 <57997566+LiKexin926@users.noreply.github.com>
2020-01-04 21:25:06 +0800

    Add files via upload

---
+++ DenseNet/test.py
@@ -0,0 +1,23 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+import csv
+from sklearn.model_selection import train_test_split
+from keras.models import load_model
+from tqdm import tqdm
+import read_d
+import pandas as pd
+
+test_path = '../test'
+csv_filepath = 'submission.csv'
+new_test = read_d.new_test(csv_filepath,test_path)
+model = load_model('model.h5')
+y_pred = model.predict(new_test)[:,1]
+
+csv_file = csv.reader(open(csv_filepath))
+name = []
+for cont in csv_file:
+    if cont[0]!='Id':
+        name.append(cont[0])
+dataframe = pd.DataFrame({'Id':name,'Predicted':y_pred})
+dataframe.to_csv('submission.csv',index=False)
---
+++ DenseNet/train.py
@@ -0,0 +1,20 @@
+from keras.callbacks import ReduceLROnPlateau, TensorBoard, ModelCheckpoint,Callback
+from densenet import get_model
+import tensorflow as tf
+import numpy as np
+import read_d
+import keras
+
+train_path = "../train_val" 
+label_path = "../train_val.csv"
+X_train_new,x_val, train_label, y_val=read_d.new_data(train_path,label_path)
+model = get_model(weights=None)
+model.compile(loss='categorical_crossentropy', optimizer='adamax',metrics=["accuracy"])
+
+filepath="model.h5"
+reduce_lr = ReduceLROnPlateau(monitor='acc', factor=0.2,patience=3, min_lr=0.001)
+checkpoint = ModelCheckpoint(filepath,monitor='val_loss',verbose=0,save_best_only=True,mode='min',period=1)
+callbacks_list=[reduce_lr,
+                checkpoint,
+               ]
+model.fit(X_train_new,train_label,batch_size=30,epochs=20,validation_data=(x_val,y_val),callbacks=callbacks_list,shuffle=True)
